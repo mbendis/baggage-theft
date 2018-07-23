8f92a1fb41293e17fcdef1c054d2953b41a6790c
mbendis <marin.bendis@gmail.com>
2018-07-23 15:01:33 +0200

    Basic state classes

---
+++ core/src/com/baggagetheft/States/HighscoreState.java
@@ -0,0 +1,4 @@
+package com.baggagetheft.States;
+
+public class HighscoreState {
+}
---
+++ core/src/com/baggagetheft/States/State.java
@@ -0,0 +1,10 @@
+package com.baggagetheft.States;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector3;
+
+public class State {
+    protected OrthographicCamera cam;
+    protected Vector3 mouse;
+
+}
