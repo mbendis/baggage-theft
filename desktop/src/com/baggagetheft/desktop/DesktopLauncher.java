6bc21c790252f090d4d5e77c1a1dfd59b13a93f0
Marin Bendis <marin.bendis@fer.hr>
2018-07-23 13:54:41 +0200

    InitialSetup

---
+++ desktop/src/com/baggagetheft/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.baggagetheft.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.baggagetheft.BaggageTheft;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BaggageTheft(), config);
+	}
+}
